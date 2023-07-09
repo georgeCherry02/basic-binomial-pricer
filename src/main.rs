4856e527d3c15b876589b250c998f8da9fefb0f5
George <georgeb.cherry@gmail.com>
2023-07-09 13:28:44 +0100

    Added explicit validation function

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use chrono::prelude::Utc;
 use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime};
 use clap::{arg, Parser};
 use log::info;
+use option::Call;
 use result::{PricerError, PricerResult};
 
 #[derive(Parser)]
@@ -19,9 +20,14 @@ pub struct Cli {
     pub apr: f64,
 }
 
-fn main() -> PricerResult<()> {
-    env_logger::init();
-    let args = Cli::parse();
+struct ValidatedInterface {
+    underlying_price: f64,
+    call: Call,
+    num_steps: u64,
+    annualised_rate: f64,
+}
+
+fn parse_cli(args: Cli) -> PricerResult<ValidatedInterface> {
     let midnight = NaiveTime::from_hms_opt(0, 0, 0).unwrap();
     let expiry_naive = NaiveDateTime::new(args.expiry, midnight);
     let expiry_dt: DateTime<Utc> = DateTime::<Utc>::from_utc(expiry_naive, Utc);
@@ -33,6 +39,18 @@ fn main() -> PricerResult<()> {
         });
     }
     let call = option::get_call(args.strike_price, args.volatility, expiry_dt);
-    info!("Received a pricing call with:\nUnderlying Price = {}\nCall = {}\nRisk-free Rate = {}\nAnd pricing with {} steps", args.underlying_price, call, args.apr, args.num_steps);
+    Ok(ValidatedInterface {
+        underlying_price: args.underlying_price,
+        call,
+        num_steps: args.num_steps,
+        annualised_rate: args.apr,
+    })
+}
+
+fn main() -> PricerResult<()> {
+    env_logger::init();
+    let args = Cli::parse();
+    let interface = parse_cli(args)?;
+    info!("Received a pricing call with:\nUnderlying Price = {}\nCall = {}\nRisk-free Rate = {}\nAnd pricing with {} steps", interface.underlying_price, interface.call, interface.annualised_rate, interface.num_steps);
     Ok(())
 }
