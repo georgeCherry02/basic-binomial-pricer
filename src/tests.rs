514f5bb78fd55e172f4bd3358b367b5d1c008881
George <georgeb.cherry@gmail.com>
2023-07-10 18:34:53 +0100

    Add basic test for both directions

--- src/tests.rs
+++ src/tests.rs
@@ -9,7 +9,7 @@ use crate::risk_free_model;
 use test_log;
 
 #[test_log::test]
-fn one_year_test() {
+fn one_year_forward_test() {
     let rfm = risk_free_model::get_annualised_risk_free_rate(5.0);
     let start: f64 = 100.0;
     let begin_date = Utc.timestamp_millis_opt(1688917143000).unwrap();
@@ -17,6 +17,19 @@ fn one_year_test() {
     let ret = rfm.apply(start, begin_date, end_date);
     let upper_bound: f64 = 105.02;
     let lower_bound: f64 = 105.01;
+    assert!(ret < upper_bound);
+    assert!(ret > lower_bound);
+}
+
+#[test_log::test]
+fn one_year_backward_test() {
+    let rfm = risk_free_model::get_annualised_risk_free_rate(5.0);
+    let start: f64 = 105.0;
+    let begin_date = Utc.timestamp_millis_opt(1720539543000).unwrap();
+    let end_date = Utc.timestamp_millis_opt(1688917143000).unwrap();
+    let ret = rfm.apply(start, begin_date, end_date);
+    let upper_bound: f64 = 99.99;
+    let lower_bound: f64 = 99.98;
     assert!(ret < upper_bound);
     assert!(ret > lower_bound);
 }
