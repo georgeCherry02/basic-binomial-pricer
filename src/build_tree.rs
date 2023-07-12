8b909af6897e15bf912c2877ddbf40161fff8679
George <georgeb.cherry@gmail.com>
2023-07-12 20:53:36 +0100

    Perhaps an implementation?

--- src/build_tree.rs
+++ src/build_tree.rs
@@ -1,14 +1,13 @@
 use chrono::prelude::Utc;
 use chrono::DateTime;
 
-use crate::option::Call;
-use crate::result::{make_not_implemented_error, PricerResult};
+use crate::result::PricerResult;
 
 pub struct Node {
-    underlying_price: f64,
-    time: DateTime<Utc>,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+    price: f64,
+    datetime: DateTime<Utc>,
+    up: Option<Box<Node>>,
+    down: Option<Box<Node>>,
 }
 
 fn get_datetime_range(
@@ -25,12 +24,59 @@ fn get_datetime_range(
     date_range
 }
 
+enum Direction {
+    DOWN,
+    UP,
+}
+
+fn make_node(
+    price: f64,
+    volatility: f64,
+    time_slot: usize,
+    date_range: &Vec<DateTime<Utc>>,
+    dir: Direction,
+) -> Option<Box<Node>> {
+    let delta = match dir {
+        Direction::UP => volatility,
+        Direction::DOWN => -volatility,
+    };
+    let new_price = price + delta;
+    date_range
+        .get(time_slot)
+        .map(|datetime: &DateTime<Utc>| -> Box<Node> {
+            Box::new(Node {
+                price: new_price,
+                datetime: datetime.clone(),
+                up: make_node(
+                    new_price,
+                    volatility,
+                    time_slot + 1,
+                    date_range,
+                    Direction::UP,
+                ),
+                down: make_node(
+                    new_price,
+                    volatility,
+                    time_slot + 1,
+                    date_range,
+                    Direction::DOWN,
+                ),
+            })
+        })
+}
+
 pub fn construct_tree(
-    call: Call,
+    underlying_price: f64,
+    vol: f64,
     start: DateTime<Utc>,
     end: DateTime<Utc>,
     num_steps: i32,
 ) -> PricerResult<Node> {
     let date_range = get_datetime_range(start, end, num_steps);
-    Err(make_not_implemented_error())
+    Ok(Node {
+        price: underlying_price,
+        datetime: start,
+        up: make_node(underlying_price, vol, 1, &date_range, Direction::UP),
+        down: make_node(underlying_price, vol, 1, &date_range, Direction::DOWN),
+    })
 }
