21136b64f70b0be53b29e2a9f8e2ab431d467e0d
GeorgeCherry02 <georgeb.cherry@gmail.com>
2024-08-23 15:42:23 +0100

    Remove unnecessary impot

--- app/__main__.py
+++ app/__main__.py
@@ -1,8 +1,7 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.graph_objects as go
-import plotly.express as px
 
-from datetime import date, datetime, time
+from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 import pytz
 
@@ -11,9 +10,9 @@ from pricer import Call, ShockLimits, generate_shock_grid
 app = Dash()
 
 app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Input(id='strike', type="number", value=50.),
-    dcc.Input(id='cost', type="number", value=5.),
+    html.H1(children='Black-Scholes Calculator', style={'textAlign':'center'}),
+    dcc.Slider(id='strike', min=20., max=100., value=50.),
+    dcc.Slider(id='cost', min=0., max=20., value=5.),
     dcc.DatePickerSingle(id="expiry", min_date_allowed=date.today(), date=date.today() + relativedelta(months=3)),
     dcc.Graph(id='graph-content')
 ]
@@ -41,8 +40,13 @@ def update_off_strike(strike, cost, expiry):
     shock_grid = get_predefined_shock_grid()
     risk_free_rate = 0.04
     valuations = shock_grid.value_black_scholes(call, risk_free_rate)
+    camera = dict(
+        up=dict(x=0, y=0, z=1),
+        center=dict(x=0, y=0, z=0),
+        eye=dict(x=-1.25, y=-1.25, z=1.25)
+    )
     fig = go.Figure(data=[go.Surface(x=shock_grid.prices(), y=shock_grid.volatilities(), z=valuations, cmid=0., colorscale=["red", "white", "green"])])
-    fig.update_layout(title="Option Plot", width=700, height=700)
+    fig.update_layout(scene_camera=camera, title="Option Plot", width=700, height=700)
     fig.update_scenes(xaxis_title_text='Price', yaxis_title_text='Volatility', zaxis_title_text='BSM Valuation')
     return fig
 
