85833eb05ea22c08739073e250caca0edb1d2176
GeorgeCherry02 <georgeb.cherry@gmail.com>
2024-08-27 17:32:49 +0100

    Refactor black-scholes tests

--- src/black_scholes/test.rs
+++ src/black_scholes/test.rs
@@ -1,19 +1,47 @@
 use super::pricing::BlackScholes;
-use crate::option::{get_call, get_put};
+use crate::option::{get_call, get_put, Call, Put};
 use crate::risk_factor::RiskFactors;
-use chrono::{TimeZone, Utc};
+use chrono::{DateTime, TimeZone, Utc};
+
+fn get_test_evaluation_period() -> (DateTime<Utc>, DateTime<Utc>) {
+    (
+        Utc.timestamp_millis_opt(1688917143000).unwrap(),
+        Utc.timestamp_millis_opt(1704697100000).unwrap(),
+    )
+}
+
+fn get_test_risk_factors() -> RiskFactors {
+    let underlying_price = 42f64;
+    let underlying_volatility = 0.2f64;
+    let risk_free_rate = 0.05f64;
+    RiskFactors {
+        underlying_price,
+        underlying_volatility,
+        risk_free_rate,
+    }
+}
+
+fn get_test_inputs_call() -> (Call, DateTime<Utc>, RiskFactors) {
+    let cost = 0f64;
+    let strike = 40f64;
+    let (begin_date, end_date) = get_test_evaluation_period();
+    let call = get_call(strike, end_date, cost);
+    let risk_factors = get_test_risk_factors();
+    (call, begin_date, risk_factors)
+}
+
+fn get_test_inputs_put() -> (Put, DateTime<Utc>, RiskFactors) {
+    let cost = 0f64;
+    let strike = 40f64;
+    let (begin_date, end_date) = get_test_evaluation_period();
+    let put = get_put(strike, end_date, cost);
+    let risk_factors = get_test_risk_factors();
+    (put, begin_date, risk_factors)
+}
 
 #[test]
 fn half_year_black_scholes_put() {
-    let underlying_price = 42f64;
-    let strike = 40f64;
-    let cost = 0f64;
-    let implied_volatility = 0.2f64;
-    let begin_date = Utc.timestamp_millis_opt(1688917143000).unwrap();
-    let end_date = Utc.timestamp_millis_opt(1704697100000).unwrap();
-    let put = get_put(strike, end_date, cost);
-    let rfr = 0.05;
-    let risk_factors = RiskFactors::new(underlying_price, implied_volatility, rfr);
+    let (put, begin_date, risk_factors) = get_test_inputs_put();
     #[allow(unused_must_use)]
     {
         put.value_black_scholes(begin_date, risk_factors, vec![])
@@ -26,15 +54,7 @@ fn half_year_black_scholes_put() {
 
 #[test]
 fn half_year_black_scholes_call() {
-    let underlying_price = 42f64;
-    let strike = 40f64;
-    let cost = 0f64;
-    let implied_volatility = 0.2f64;
-    let begin_date = Utc.timestamp_millis_opt(1688917143000).unwrap();
-    let end_date = Utc.timestamp_millis_opt(1704697100000).unwrap();
-    let call = get_call(strike, end_date, cost);
-    let rfr = 0.05;
-    let risk_factors = RiskFactors::new(underlying_price, implied_volatility, rfr);
+    let (call, begin_date, risk_factors) = get_test_inputs_call();
     #[allow(unused_must_use)]
     {
         call.value_black_scholes(begin_date, risk_factors, vec![])
